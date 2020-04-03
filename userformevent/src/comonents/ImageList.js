1d6f127e7f639d46ef97171117c911670907c322
isha2510 <ishatripathi80@gmail.com>
2020-04-03 12:12:11 +0530

    adding image card and css

--- userformevent/src/comonents/ImageList.js
+++ userformevent/src/comonents/ImageList.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList=(props)=>{
 
-    const images =props.images.map(({description,urls,id})=>{
+    const images =props.images.map((image)=>{
         //return <img alt={image.description} key={image.id} src={image.urls.regular}/>
-        return <img alt={description} key={id} src={urls.regular}/>
+        return <ImageCard  key={image.id} image ={image}/>
     });
 
     
     return(
-        <div>{images}</div>
+        <div className="image-list">{images}</div>
     );
 };
 
